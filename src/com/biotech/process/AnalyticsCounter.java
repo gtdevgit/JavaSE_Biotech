d2f4af93ab0483bcd70462a5a9c7aecee9e6e07d
Guillaume Toussaint <gtdev.git@gmail.com>
2022-02-10 14:29:48 +0100

    Ajout d'un message quand le fichier des symptomes est introuvale.

--- src/com/biotech/process/AnalyticsCounter.java
+++ src/com/biotech/process/AnalyticsCounter.java
@@ -2,20 +2,29 @@ package com.biotech.process;
 
 import com.biotech.process.model.ListeSymptome;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
+
+	private static String getInputFilePatch(){
+		return "symptoms.txt";
+	}
+
 	public static void main(String args[]) throws Exception {
 		ListeSymptome listeSymptome = new ListeSymptome();
 
+		String inputFile = getInputFilePatch();
+		File file = new File(inputFile);
+		if (!file.exists()){
+			System.err.println(String.format("Fichier introuvable : %s", file.getAbsolutePath()));
+			return;
+		}
+
 		// first get input
-		try (BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
+		try (BufferedReader reader = new BufferedReader (new FileReader(inputFile))) {
 			String line = reader.readLine();
 			while (line != null) {
 				listeSymptome.ajouter(line);
